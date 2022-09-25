4c3d648fcc608ace6b27795177ca86298c56a7a1
Nyak Gymnastiar Syah <104628844+nyakgym@users.noreply.github.com>
2022-09-25 20:00:59 +0700

    Add files via upload

---
+++ praktikum2/Break.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+        <?php
+            // melakukan break pada $i == 2
+            for($i = 0; $i<5; $i++)
+            {
+                if($i == 2)
+                {
+                    break;
+                }
+                echo("Nilai i : $i <br>");
+            }
+            echo("Loop Selesai");
+        ?>
+    </body>
+</html>   
